da7ab9698811433052115f77ce3f7579de561007
ToiLaKiet <tuilakietday05@gmail.com>
2025-12-27 09:12:35 +0700

    first edit

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,9 @@ import type { IconMap, SocialLink, Site } from '@/types'
 export const SITE: Site = {
   title: 'Toi La Kiet',
   description:
-    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  href: 'https://astro-erudite.vercel.app',
-  author: 'jktrn',
+    'When life goes hard, i would go hehe',
+  href: 'https://toilakiet.github.io',
+  author: 'Vo Anh Kiet',
   locale: 'en-US',
   featuredPostCount: 2,
   postsPerPage: 3,
@@ -28,20 +28,12 @@ export const NAV_LINKS: SocialLink[] = [
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    href: 'https://github.com/jktrn',
+    href: 'https://github.com/ToiLaKiet',
     label: 'GitHub',
   },
   {
-    href: 'https://twitter.com/enscry',
-    label: 'Twitter',
-  },
-  {
-    href: 'mailto:jason@enscribe.dev',
+    href: 'mailto:toilakiet.dev@gmail.com',
     label: 'Email',
-  },
-  {
-    href: '/rss.xml',
-    label: 'RSS',
   },
 ]
 
